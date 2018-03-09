28d3ee9ac52852701604f8cc6f7b5aad1af6ef29
Sergeyakzhik <sergeyakzhik@gmail.com>
2018-03-09 17:47:14 +0300

    Com 12

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+    var N = 1;
+
+    for(let i = 0; i < pairs.length; i++) {
+        if(pairs[i][1] > 10)
+            return 0;
+        N *= Math.pow(pairs[i][0], pairs[i][1]);
+        if(N > 100000)
+            return 0;
+    }
+
+    var arr = [];
+
+    for(let i = 0; i < N; i++){
+        arr[i] = i;
+    }
+
+    for(let j = 0; j < s.length; j++){
+
+        var b = check(j, s, N, arr);
+        arr.splice(0, arr.length);
+        arr = b;
+    }
+
+    return arr.length % 1000000007;
+}
+
+
+function check (j, s, N, arr) {
+    var a = [];
+    for(let i = 0; i < arr.length; i++) {
+        if ((findGCD(arr[i] + j, N) != 1 && s[j] == 0) || (findGCD(arr[i] + j, N) == 1 && s[j] == 1)) {
+            a.push(arr[i]);
+        }
+    }
+    return a;
+}
+
+function findGCD (a, b) {
+    if (!b) {
+        return a;
+    }
+
+    return findGCD(b, a % b);
 }
